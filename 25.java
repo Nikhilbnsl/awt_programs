e686accecd3b5dad522315b28b46459e24080a65
Nikhil Bansal <97783351+Nikhilbnsl@users.noreply.github.com>
2022-07-13 14:34:45 +0530

    commits

---
+++ 25.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+import java.applet.*;
+
+public class MyAppBan extends Applet implements Runnable 
+{
+   	String msg="   <* Banner *>   ";
+  	Thread t=null;
+   	int state;
+   	boolean stopflag;
+
+   	public void init() 
+   	{
+   		setSize(300,100);
+   		t=new Thread(this);
+   		try
+   		{
+   			t.start();
+   			t.suspend();
+		} 
+   		catch (Exception e) 
+   		{
+   			System.out.println("Error : "+ e);
+		}
+  	}
+
+   	public void start ()
+   	{
+   		stopflag = false;
+   		t.resume();
+   	}
+
+   	public void run() 
+   	{
+   		char ch;
+   		while(true)
+   		{
+   			try
+      			{
+         				repaint();
+          				Thread.sleep(250);
+         				ch = msg.charAt(0);
+          				msg = msg.substring(1,msg.length());
+          				msg += ch;
+         				if(stopflag)
+             				break;
+         			} 
+			catch(Exception e)
+			{
+				System.out.println("Error : "+ e);
+			}
+	   	}
+	}
+
+	public void stop()
+	{
+	   	stopflag=true;
+	   	try
+   		{
+			t.suspend();
+		} 
+   		catch (Exception e) 
+   		{
+   			System.out.println("Error : "+ e);
+		}
+	
+	}
+
+	public void paint(Graphics g) 
+	{
+	   	g.drawString(msg,50,30);
+	}
+}
